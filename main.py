95445d56fd78c75234bc9c21c48ed54ff567b1d1
Maxim Kozlov <maximko.ca@gmail.com>
2026-05-10 21:13:49 -0400

    Button in ui

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.anchorlayout import AnchorLayout
 kivy.require('1.9.0')
 
 class AnyStreak(App):
     def build(self):
-        return BoxLayout()
+        return AnchorLayout()
     
 
 anystreak = AnyStreak()
